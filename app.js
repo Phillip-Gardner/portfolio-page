dd8985a160b1f9745306cf55ef396bf5d677232d
Phillip Gardner <pbg.gardner@gmail.com>
2021-03-17 18:51:33 +0000

    adding about section

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  font-family: 'Kumbh Sans', sans-serif;
-}
+  const menu = document.querySelector("#mobile-menu")
+  const menuLinks = document.querySelector(".navbar_menu")
+
+  // Display Mobile Menu
+  // Function that adds active class like in our CSS
+  const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+  }
+
+  menu.addEventListener('click', mobileMenu);
