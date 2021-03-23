abf792954541638726f87f5fce90925879337e1d
Phillip Gardner <pbg.gardner@gmail.com>
2021-03-23 21:31:55 +0000

    added scroll feature in navbar

--- app.js
+++ app.js
@@ -54,3 +54,13 @@ const hideMobileMenu = () => {
 
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {scrollIndicator()};
+
+function scrollIndicator() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
